b249d81515f74741b3d09e437cedb9085800be8f
Joakim Pedersen Nilfjord <joakim.nilfjord@gmail.com>
2017-03-23 21:49:26 +0700

    reformatted files

--- src/main/java/io/muic/ooc/controller/ViewItemsController.java
+++ src/main/java/io/muic/ooc/controller/ViewItemsController.java
@@ -5,13 +5,11 @@ import io.muic.ooc.model.User;
 import io.muic.ooc.service.ProductService;
 import io.muic.ooc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 
 import java.util.ArrayList;
 
